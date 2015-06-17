7df00825ed26e650b3acb27326e94ae7c15fb7fd
Michael <mmcwilliam@gmail.com>
2015-06-17 13:46:04 -0600

    does it work?

--- test/integration/reporters/progress_reporter_test.rb
+++ test/integration/reporters/progress_reporter_test.rb
@@ -8,16 +8,16 @@ module MinitestReportersTest
       output = `ruby #{test_filename} 2>&1`
       
       assert_match /ERROR(\033\[0m)*\["test_error"/, output, 'Errors should be displayed'
-      assert_match 'FAIL(\\e)?\[(0m\[)?\\"test_failure"', output, 'Failures should be displayed'
-      assert_match /SKIP(\\e)?\[(0m\[)?\\"test_skip/, output, 'Skipped tests should be displayed'
+      assert_match /FAIL(\033\[0m)*\["test_failure"/, output, 'Failures should be displayed'
+      assert_match /SKIP(\033\[0m)*\["test_skip"/, output, 'Skipped tests should be displayed'
     end
     def test_skipped_tests_are_not_displayed
       fixtures_directory = File.expand_path('../../../fixtures', __FILE__)
       test_filename = File.join(fixtures_directory, 'progress_detailed_skip_test.rb')
       output = `ruby #{test_filename} 2>&1`
-      assert_match 'ERROR["test_error"', output, 'Errors should be displayed'
-      assert_match 'FAIL["test_failure"', output, 'Failures should be displayed'
-      refute_match 'SKIP(\e)?[(0m[)?"test_skip', output, 'Skipped tests should not be displayed'
+      assert_match /ERROR(\033\[0m)*\["test_error"/, output, 'Errors should be displayed'
+      assert_match /FAIL(\033\[0m)*\["test_failure"/, output, 'Failures should be displayed'
+      refute_match /SKIP(\033\[0m)*\["test_skip"/, output, 'Skipped tests should not be displayed'
     end
   end
 end
